c28e6550138739d69f3b61907e51bd4926cacfe1
Josue <josuenahumap@gmail.com>
2026-09-18 18:14:44 +0000

    ✨ Feat: Enhance app.js Enhance app.js With middleware configuration and error handling comments; update www for debug message; add greet script in package.json; include itgam image

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
+//funcion para mejerar errores en la aplicación
 var createError = require('http-errors');
+//importa el framework express
 var express = require('express');
+//importa modulos para manejar rutas, cookies, logs y path
 var path = require('path');
+//importa modulos para manejar cookies
 var cookieParser = require('cookie-parser');
+//importa modulos para manejar logs
 var logger = require('morgan');
 
+//importa las rutas de laaplicacion 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//crea la aplicacion express
 var app = express();
 
-// view engine setup
+// configura el motor de vistas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//configura los middlewares de la aplicacion (software intermedio que se ejecuta antes de las rutas)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//configura la carpeta de archivos estaticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
